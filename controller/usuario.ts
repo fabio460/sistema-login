e271af9c3ca1eefc8369c80d7e9f6ad7aedb9f39
Fabio <dasilvadeoliveirafabio@email.com>
2026-07-03 19:01:38 -0300

    teste deploy 4

--- controller/usuario.ts
+++ controller/usuario.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import { Request, Response } from "express";
-import { PrismaClient } from "../generated/prisma/client";
+//import { PrismaClient } from "../generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export const getUsuario = async(req:Request, res:Response) =>{
