2d8ed8f0944626b42f720e6b9e2e852037baf282
Fabio <dasilvadeoliveirafabio@email.com>
2026-07-06 17:47:48 -0300

    teste 2

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import express from "express";
-import { getUsuario } from './controller/usuario'
+import cors from "cors";
+import { getUsuario } from "./controller/usuario.js";
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.get("/usuario",getUsuario)
 app.listen(4000,()=>console.log("rodando na porta 4000"))
